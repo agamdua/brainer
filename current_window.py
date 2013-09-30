65927ca57118e5012846f176ce76e7f88cdc01a7
Agam Dua <agamdua@agamdua-HCL-Desktop.(none)>
2013-09-30 20:11:19 +0530

    Partial Fix: Outputs full string + variable name
    Current ouput example:
    
    1380571607
    ------------
    _NET_WM_NAME(UTF8_STRING) = "XPROP(1) manual page - Mozilla Firefox"
    
    TODO: slice the text so that the variable name is not part of output.

--- current_window.py
+++ current_window.py
@@ -1,6 +1,9 @@
 import subprocess, time, datetime, calendar, shlex
 
-monitor_cmd = 'xprop -id $(xprop -root -f _NET_ACTIVE_WINDOW 0x " \$0\\n" _NET_ACTIVE_WINDOW | awk "{print \$2}") | grep "_NET_WM_NAME(UTF8_STRING)" | awk "{print \$3, \$4}"'
+# monitor_cmd = 'xprop -id $(xprop -root -f _NET_ACTIVE_WINDOW 0x " \$0\\n" _NET_ACTIVE_WINDOW | awk "{print \$2}") | grep "_NET_WM_NAME(UTF8_STRING)" | awk "{print \$3, \$4}"'
+monitor_cmd = 'xprop -id $(xprop -root -f _NET_ACTIVE_WINDOW 0x " \$0\\n" _NET_ACTIVE_WINDOW | awk "{print \$2}") | grep "_NET_WM_NAME(UTF8_STRING)"'
+
+
 
 def unix_timestamp():
     present_time = datetime.datetime.now()
