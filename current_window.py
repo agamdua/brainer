bb3dceae742c8ee277fa9805534266300be7ffbe
Agam Dua <agamdua@agamdua-HCL-Desktop.(none)>
2013-09-30 19:55:29 +0530

    First Commit: Created working prototype
    Script polls every 5 seconds to find out the current window and prints title
    as string.
    
    Known bug: Full string is not printed in Firefox.
    
    TODO: Test with other applications as well.

---
+++ current_window.py
@@ -0,0 +1,17 @@
+import subprocess, time, datetime, calendar, shlex
+
+monitor_cmd = 'xprop -id $(xprop -root -f _NET_ACTIVE_WINDOW 0x " \$0\\n" _NET_ACTIVE_WINDOW | awk "{print \$2}") | grep "_NET_WM_NAME(UTF8_STRING)" | awk "{print \$3, \$4}"'
+
+def unix_timestamp():
+    present_time = datetime.datetime.now()
+    return calendar.timegm(present_time.utctimetuple())
+
+
+while True:
+    title_subprocess = subprocess.Popen(monitor_cmd, stdout=subprocess.PIPE, shell=True)
+    title_sp_comm = title_subprocess.communicate()
+    output = title_sp_comm[0]
+    print output
+    print unix_timestamp()
+    print "------------"
+    time.sleep(5)
